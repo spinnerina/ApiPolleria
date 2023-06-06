6438579515da14787b81596104b22d91088cd0e6
spinnerina <mati.fessia.2001@gmail.com>
2023-06-05 23:17:40 -0300

    Pequeños Cambios

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -15,9 +15,9 @@ class UsuarioController extends Controller
         $user = Usuario::where('usu_login', $credentials['usu_login'])->first();
         
         if ($user && Hash::check($credentials['usu_contrasenia'], $user->usu_contrasenia)) {
-            return response()->json(['message' => 'Logged in successfully.']);
+            return response()->json(['message' => 'Login Correcto', 'usuario' => $user]);
         } else {
-            return response()->json(['message' => 'Invalid credentials.'], 401);
+            return response()->json(['message' => 'Credenciales invalidas.'], 401);
         }
     }
 }
