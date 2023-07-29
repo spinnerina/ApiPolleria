fb455d115edee331453e1880051a6a87786ff251
Matias <mati.fessia.2001@gmail.com>
2023-07-29 11:33:15 -0300

    Cambio de pc 29/07/2023

---
+++ database/migrations/2023_07_29_061835_create_productos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id('prod_id');
+            $table->string('prod_cod');
+            $table->string('prod_descri');
+            $table->string('prod_cod_barra')->nullable();
+            $table->decimal('prod_precio_lista', 10, 2); 
+            $table->string('prod_imagen')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('prov_id')->nullable();
+
+            $table->foreign('prov_id')
+                  ->references('prov_id')
+                  ->on('Proveedor')
+                  ->onDelete('set null');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
